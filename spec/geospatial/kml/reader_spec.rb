26432989973795aa59afa45a9d8545282b13627f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-03-04 01:01:01 +1300

    Tidy up specs.

--- spec/geospatial/kml/reader_spec.rb
+++ spec/geospatial/kml/reader_spec.rb
@@ -1,15 +1,11 @@
-require "spec_helper"
-require 'irb'
 
 require 'geospatial/kml/reader'
 
-RSpec.describe Geospatial::KML do
+RSpec.describe Geospatial::KML::Reader do
 	let(:kml_path) {File.join(__dir__, "christchurch.kml")}
 	let(:reader) {Geospatial::KML::Reader.load_file(kml_path)}
 	
 	it "should have one polygon" do
-		binding.irb
-		
 		expect(reader.polygons.count).to be == 1
 	end
 end
