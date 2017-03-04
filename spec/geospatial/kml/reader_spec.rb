d8265c693397d2355302c43d2162c34a570d2cd8
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-03-04 14:29:09 +1300

    Check that polygon is well formed.

--- spec/geospatial/kml/reader_spec.rb
+++ spec/geospatial/kml/reader_spec.rb
@@ -4,9 +4,11 @@ require 'geospatial/kml/reader'
 RSpec.describe Geospatial::KML::Reader do
 	let(:kml_path) {File.join(__dir__, "christchurch.kml")}
 	let(:reader) {Geospatial::KML::Reader.load_file(kml_path)}
+	let(:center) {Vector[172.638216, -43.526326]}
 	
 	it "should have one polygon" do
 		expect(reader.polygons.count).to be == 1
 		expect(reader.polygons.first).to be_kind_of Geospatial::Polygon
+		expect(reader.polygons.first).to be_include_point center
 	end
 end
